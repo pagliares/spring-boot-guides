41351537a089814ac87034d8a5b722588419dd91
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-12-22 12:52:56 -0300

    Create a controller to the welcome page

---
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "Greetings form Spring Boot!";
+    }
+}
