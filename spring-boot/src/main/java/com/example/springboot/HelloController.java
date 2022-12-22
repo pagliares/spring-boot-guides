7756482780d5ec51d2159ea784c4047f9117bca3
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-12-22 14:14:36 -0300

    Create test that mocks the servlet request and response

--- spring-boot/src/main/java/com/example/springboot/HelloController.java
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index(){
-        return "Greetings form Spring Boot!";
+        return "Greetings from Spring Boot!";
     }
 }
