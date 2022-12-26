da71ae4d89fd4bc0c439ea815b8453f8a0ab29c5
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-12-26 12:07:33 -0300

    Create example that integrates Spring-boot with MySQL

--- accessing-data-mysql/src/main/java/com/example/integration/mysql/AccessingDataMysqlApplication.java
+++ accessing-data-mysql/src/main/java/com/example/integration/mysql/AccessingDataMysqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccessingDataMysqlApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
 
