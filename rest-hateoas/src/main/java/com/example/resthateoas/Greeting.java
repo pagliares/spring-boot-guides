04fc6f3113855be165631fb65b9d4a2112451ad1
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-12-30 17:29:48 -0300

    Create rest-hateoas example

---
+++ rest-hateoas/src/main/java/com/example/resthateoas/Greeting.java
@@ -0,0 +1,20 @@
+package com.example.resthateoas;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Greeting extends RepresentationModel<Greeting> {
+
+    private final String content;
+
+    @JsonCreator
+    public Greeting(@JsonProperty("content") String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ rest-hateoas/src/main/java/com/example/resthateoas/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.resthateoas;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class GreetingController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+
+    @RequestMapping("/greeting")
+    public HttpEntity<Greeting> greeting(
+            @RequestParam(value = "name", defaultValue = "World") String name) {
+
+        Greeting greeting = new Greeting(String.format(TEMPLATE, name));
+        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+
+        return new ResponseEntity<>(greeting, HttpStatus.OK);
+    }
+}
