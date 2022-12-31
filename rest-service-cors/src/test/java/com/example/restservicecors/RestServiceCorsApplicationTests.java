73f67514fde0db7f1d3707f9fd1b597ebf8171d6
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-12-31 13:37:02 -0300

    Create rest-service-cors example

---
+++ rest-service-cors/src/test/java/com/example/restservicecors/RestServiceCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservicecors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceCorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
