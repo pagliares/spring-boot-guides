e41bf221199f3fc790e89ec5da924ce0d53a2867
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-01-05 20:15:02 -0300

    Create securing-web example

---
+++ securing-web/src/test/java/com/example/securingweb/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
