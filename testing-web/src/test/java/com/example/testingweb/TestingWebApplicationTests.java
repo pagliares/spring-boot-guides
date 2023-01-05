b19071a18bc9f7f3a1eb2a8f70259aebc0fbd2d9
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2023-01-05 10:52:07 -0300

    Create example testing-web

---
+++ testing-web/src/test/java/com/example/testingweb/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
